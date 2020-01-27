662854a6772b8d906a0c3e24ea509419ec8c2e15
Aaron Jacobs <atheriel@gmail.com>
2020-01-26 22:45:33 -0500

    Creates a public R6 interface.
    Since providers have inherent reference semantics, it makes sense to go
    this route as opposed to having a (more) confusing S3 object which is
    simply mutated by reference.

--- R/package.R
+++ R/package.R
@@ -1,37 +1,87 @@
-usdt_provider <- function(name) {
-  stopifnot(is.character(name))
-  ptr <- .Call(R_usdt_provider, name, PACKAGE = "usdt")
-  structure(list(ptr = ptr, name = name), class = "usdt_provider")
-}
+#' USDT Providers
+#'
+#' @details
+#'
+#' User-level Statically Defined Tracing (USDT) consists of a "provider" and one
+#' or more "probes". Probes can be "fired", in which case R will emit an event
+#' -- but only if a tracer (such as BPF on Linux or Dtrace on Solaris and macOS)
+#' is listening.
+#'
+#' USDT is a way to add opt-in runtime instrumentation to your application with
+#' very little overhead.
+#'
+#' @examples
+#'
+#' p <- Provider$new("my-r-code")
+#' p$add_probe("add-name", integer(), character(), character())
+#' p$enable()
+#'
+#' # Fire an event. This will return FALSE, because nothing is
+#' # listening at the moment.
+#' p$fire("add-name", function() list(1L, "First", "Last"))
+#'
+#' @export
+Provider <- R6::R6Class(
+  "UsdtProvider",
+  cloneable = FALSE,
+  public = list(
+    #' @param name A name for the provider.
+    initialize = function(name) {
+      stopifnot(is.character(name))
+      private$ptr <- .Call(R_usdt_provider, name, PACKAGE = "usdt")
+      private$name <- name
+      private$enabled <- FALSE
+    },
 
-print.usdt_provider <- function(x, ...) {
-  enabled <- .Call(R_usdt_provider_is_enabled, x$ptr, PACKAGE = "usdt")
-  cat("USDT Provider: ", x$name, if (enabled) " (loaded)", "\n", sep = "")
-}
+    #' @details Enable the provider, which makes it possible to fire probes.
+    #'   New probes cannot be added while the provider is enabled.
+    enable = function() {
+      .Call(R_usdt_provider_enable, private$ptr, PACKAGE = "usdt")
+      private$enabled <- TRUE
+    },
 
-usdt_provider_enable <- function(provider) {
-  stopifnot(inherits(provider, "usdt_provider"))
-  .Call(R_usdt_provider_enable, provider$ptr, PACKAGE = "usdt")
-  provider
-}
+    #' @details Disable the provider.
+    disable = function() {
+      .Call(R_usdt_provider_disable, private$ptr, PACKAGE = "usdt")
+      private$enabled <- FALSE
+    },
 
-usdt_provider_disable <- function(provider) {
-  stopifnot(inherits(provider, "usdt_provider"))
-  .Call(R_usdt_provider_disable, provider$ptr, PACKAGE = "usdt")
-  provider
-}
+    #' @details Add a probe. Probes generally refer to a specific event -- e.g.
+    #'   "request-start" or "item-updated" -- and can take up to six parameters.
+    #'
+    #' @param name A name for the probe.
+    #' @param ... R types for parameters that will be passed to the probe when
+    #'   fired. For example, \code{integer()} or \code{character()}.
+    add_probe = function(name, ...) {
+      stopifnot(is.character(name))
+      probe <- .External(R_usdt_add_probe, private$ptr, name, ..., PACKAGE = "usdt")
+      private$probes[[name]] <- probe
+    },
 
-usdt_add_probe <- function(provider, name, ...) {
-  stopifnot(inherits(provider, "usdt_provider"))
-  ptr <- .External(R_usdt_add_probe, provider$ptr, name, ..., PACKAGE = "usdt")
-  structure(list(ptr = ptr, name = name), class = "usdt_probe")
-}
-
-usdt_fire_probe <- function(probe, fun) {
-  stopifnot(inherits(probe, "usdt_probe"))
-  stopifnot(is.function(fun))
-  .Call("R_usdt_fire_probe", probe$ptr, fun, parent.frame(), PACKAGE = "usdt")
-}
+    #' @details Fire a probe. If and only if a tracer is listening, this will
+    #'   call the passed function and cause R to emit an event containing the
+    #'   parameters it retuns. The number of parameters must match the call to
+    #'   \code{add_probe()}.
+    #'
+    #' @param probe The name of a probe.
+    #' @param fun A function that when called will return a list of parameters to
+    #'   be emitted by R.
+    fire = function(probe, fun) {
+      p <- private$probes[[probe]]
+      if (is.null(p)) {
+        stop("'", probe, "' does not name a known probe.")
+      }
+      stopifnot(is.function(fun))
+      .Call("R_usdt_fire_probe", p, fun, parent.frame(), PACKAGE = "usdt")
+    }
+  ),
+  private = list(
+    name = character(),
+    ptr = NULL,
+    enabled = FALSE,
+    probes = setNames(list(), character())
+  )
+)
 
 #' @useDynLib usdt, .registration = TRUE
 NULL
--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -1,42 +1,35 @@
-testthat::test_that("Providers behalve as expected", {
-  p <- usdt_provider("testthat")
+testthat::test_that("Providers behave as expected", {
+  p <- Provider$new("testthat")
+  testthat::expect_silent(p$disable())
 
-  testthat::expect_silent(usdt_provider_disable(p))
-  testthat::expect_silent(usdt_provider_enable(p))
-  testthat::expect_silent(usdt_provider_enable(p))
-  testthat::expect_silent(usdt_provider_disable(p))
-})
-
-testthat::test_that("Probes behalve as expected", {
-  p <- usdt_provider("testthat")
-  probe <- usdt_add_probe(p, "p1", integer(), logical(), character(), numeric())
+  p$add_probe("p1", integer(), logical(), character(), numeric())
 
   cb <- function() list(2L, FALSE, "string", 1.267)
-  testthat::expect_false(usdt_fire_probe(probe, cb))
+  testthat::expect_false(p$fire("p1", cb))
 
   testthat::expect_error(
-    usdt_add_probe(
-      p, "p2", integer(), integer(), integer(), integer(), integer(), integer(),
+    p$add_probe(
+      "p2", integer(), integer(), integer(), integer(), integer(), integer(),
       integer()
     ),
     regexp = "Probes cannot accept more than 6 arguments at present"
   )
 
   testthat::expect_error(
-    usdt_add_probe(p, "p2", raw(), as.Date(Sys.time())),
+    p$add_probe("p2", raw()),
     regexp = "Can't pass R 'raw' objects to a probe."
   )
 
   testthat::expect_error(
-    usdt_add_probe(p, "p2", complex()),
+    p$add_probe("p2", complex()),
     regexp = "Can't pass R 'complex' objects to a probe."
   )
 
-  usdt_provider_enable(p)
+  p$enable()
   testthat::expect_error(
-    usdt_add_probe(p, "p2", 1),
+    p$add_probe("p2", 1),
     regexp = "Probes cannot be added while the provider is loaded"
   )
 
-  testthat::expect_false(usdt_fire_probe(probe, cb))
+  testthat::expect_false(p$fire("p1", cb))
 })
