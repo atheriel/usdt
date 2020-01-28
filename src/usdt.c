00eebc6fd4a95ba5532e28929f3a30277281ca15
Aaron Jacobs <atheriel@gmail.com>
2020-01-28 01:01:50 -0500

    Adds a libstapsdt <-> libusdt shim and refactors to use it.
    Plus, for Windows we now have a no-op shim.

--- src/usdt.c
+++ src/usdt.c
@@ -1,10 +1,137 @@
 #include <stdlib.h> /* for malloc */
 #include <stdint.h> /* for uint64_t */
 #include <Rinternals.h>
+
+#ifdef __linux
 #include "libstapsdt.h"
+#define HAS_LIBSTAPSDT
+#elif !defined(_WIN32)
+#include "usdt.h"
+#define HAS_LIBUSDT
+#endif
+
+/*
+ *  libstapsdt <-> libusdt shim.
+ *
+ *  In my view, libusdt has the nicer API, so prefer that when possible.
+ */
+
+#ifdef HAS_LIBUSDT
+
+#define probe_t usdt_probedef_t
+#define provider_t usdt_provider_t
+#define arg_t char *
+
+size_t probe_argc(probe_t *probe)
+{
+  return probe->argc;
+}
+
+int probeIsEnabled(probe_t *probe)
+{
+  return usdt_is_enabled(probe->probe);
+}
+
+probe_t * providerAddProbe(provider_t *provider, const char *name, int argCount,
+                           ...)
+{
+  va_list ap;
+  va_start(ap, argCount);
+  arg_t args[6] = {0};
+  for(int i = 0; i < argCount; i++) {
+    args[i] = va_arg(ap, arg_t);
+  }
+  probe_t *probe = usdt_create_probe(name, name, (size_t) argCount,
+                                     (const char **) &args);
+  if (probe) {
+    usdt_provider_add_probe(provider, probe);
+  }
+  return probe;
+}
+
+void probeFire(probe_t *probe, ...)
+{
+  va_list ap;
+  va_start(ap, probe);
+  uint64_t args[6] = {0};
+  for(int i = 0; i < probe->argc; i++) {
+    args[i] = va_arg(ap, uint64_t);
+  }
+
+  usdt_fire_probe(probe->probe, probe->argc, (void *) &args);
+}
+
+#elif defined(HAS_LIBSTAPSDT)
+
+#define probe_t SDTProbe_t
+#define provider_t SDTProvider_t
+#define arg_t ArgType_t
+
+size_t probe_argc(probe_t *probe)
+{
+  return probe->argCount;
+}
+
+provider_t * usdt_create_provider(const char *name, const char *module)
+{
+  return providerInit(name);
+}
+
+void usdt_provider_free(provider_t *provider)
+{
+    providerDestroy(provider);
+}
+
+int usdt_provider_enable(provider_t *provider)
+{
+  return providerLoad(provider);
+}
+
+int usdt_provider_disable(provider_t *provider)
+{
+  return providerUnload(provider);
+}
+
+char * usdt_errstr(provider_t *provider)
+{
+  return provider->error;
+}
+
+#else /* No-op implementation shim. */
+
+typedef struct provider_impl {
+  int dummy;
+} provider_t;
+
+#define probe_t provider_t
+#define arg_t char *
+
+provider_t * usdt_create_provider(const char *name, const char *module)
+{
+  provider_t *out = malloc(sizeof(provider_t));
+  return out;
+}
+
+void usdt_provider_free(provider_t *provider)
+{
+    free(provider);
+}
+
+int usdt_provider_enable(provider_t *provider) { return 0; }
+int usdt_provider_disable(provider_t *provider) { return 0; }
+int probeIsEnabled(probe_t *probe) { return 0; }
+char * usdt_errstr(provider_t *provider) { return ""; }
+
+probe_t * providerAddProbe(provider_t *provider, const char *name, int argCount,
+                           ...)
+{
+  return provider;
+}
+
+#endif /* Shim. */
 
 struct provider {
-  SDTProvider_t *provider;
+  provider_t *provider;
   int loaded;
 };
 
@@ -13,7 +140,8 @@ static void R_finalize_provider(SEXP ptr)
   Rprintf("In the finalizer...\n");
   struct provider *p = (struct provider *) R_ExternalPtrAddr(ptr);
   if (p) {
-    providerDestroy(p->provider);
+    usdt_provider_disable(p->provider);
+    usdt_provider_free(p->provider);
     p->provider = NULL;
     free(p);
     p = NULL;
@@ -26,7 +154,7 @@ SEXP R_usdt_provider(SEXP name)
   const char *name_str = CHAR(Rf_asChar(name));
   struct provider *p = malloc(sizeof(struct provider));
   p->loaded = 0;
-  p->provider = providerInit(name_str);
+  p->provider = usdt_create_provider(name_str, name_str);
   if (!p->provider) {
     Rf_error("Failed to create USDT provider.\n");
   }
@@ -56,8 +184,8 @@ SEXP R_usdt_provider_enable(SEXP ptr)
     /* Already loaded. */
     return R_NilValue;
   }
-  if (providerLoad(p->provider) < 0) {
-    Rf_error("Failed to enable USDT provider: %s.\n", p->provider->error);
+  if (usdt_provider_enable(p->provider) < 0) {
+    Rf_error("Failed to enable USDT provider: %s.\n", usdt_errstr(p->provider));
   }
   p->loaded = 1;
 
@@ -74,25 +202,33 @@ SEXP R_usdt_provider_disable(SEXP ptr)
     /* Already unloaded. */
     return R_NilValue;
   }
-  if (providerUnload(p->provider) < 0) {
-    Rf_error("Failed to disable USDT provider: %s.\n", p->provider->error);
+  if (usdt_provider_disable(p->provider) < 0) {
+    Rf_error("Failed to disable USDT provider: %s.\n", usdt_errstr(p->provider));
   }
   p->loaded = 0;
 
   return R_NilValue;
 }
 
-ArgType_t usdt_argtype_from_sexp(SEXP arg)
+arg_t usdt_argtype_from_sexp(SEXP arg)
 {
   switch(TYPEOF(arg)) {
   case LGLSXP:
     /* fallthrough */
   case INTSXP:
+#ifdef HAS_LIBSTAPSDT
     return int32;
+#else
+    return "int";
+#endif
   case STRSXP:
     /* fallthrough */
   case REALSXP:
+#ifdef HAS_LIBSTAPSDT
     return uint64;
+#else
+    return "char *";
+#endif
   default:
     Rf_error("Can't pass R '%s' objects to a probe.", Rf_type2char(TYPEOF(arg)));
   }
@@ -116,7 +252,7 @@ SEXP R_usdt_add_probe(SEXP args)
   if (arg_count > 6) {
     Rf_error("Probes cannot accept more than 6 arguments at present.");
   }
-  SDTProbe_t *probe;
+  probe_t *probe;
   SEXP first;
 
   switch (arg_count) {
@@ -171,9 +307,12 @@ SEXP R_usdt_add_probe(SEXP args)
     Rf_error("Failed to create USDT probe.\n");
   }
 
-  SEXP ptr = PROTECT(R_MakeExternalPtr(probe, R_NilValue, R_NilValue));
-  UNPROTECT(1);
-  return ptr;
+#if defined(HAS_LIBSTAPSDT) || defined(HAS_LIBUSDT)
+  return R_MakeExternalPtr(probe, R_NilValue, R_NilValue);
+#else
+  /* Dummy value. */
+  return Rf_ScalarLogical(TRUE);
+#endif
 }
 
 uint64_t usdt_arg_from_sexp(SEXP arg)
@@ -198,7 +337,8 @@ uint64_t usdt_arg_from_sexp(SEXP arg)
 
 SEXP R_usdt_fire_probe(SEXP ptr, SEXP fun, SEXP env)
 {
-  SDTProbe_t *probe = (SDTProbe_t *) R_ExternalPtrAddr(ptr);
+#if defined(HAS_LIBSTAPSDT) || defined(HAS_LIBUSDT)
+  probe_t *probe = (probe_t *) R_ExternalPtrAddr(ptr);
   if (ptr == R_NilValue || !probe) {
     Rf_error("Invalid USDT probe.\n");
   }
@@ -212,9 +352,9 @@ SEXP R_usdt_fire_probe(SEXP ptr, SEXP fun, SEXP env)
              Rf_type2char(TYPEOF(args)));
   }
   size_t arg_count = Rf_length(args);
-  if (arg_count != probe->argCount) {
+  if (arg_count != probe_argc(probe)) {
     Rf_error("Invalid number of probe arguments. Expected %d, got %d.\n",
-             probe->argCount, arg_count);
+             probe_argc(probe), arg_count);
   }
 
   switch (arg_count) {
@@ -258,6 +398,10 @@ SEXP R_usdt_fire_probe(SEXP ptr, SEXP fun, SEXP env)
 
   UNPROTECT(2);
   return Rf_ScalarLogical(TRUE);
+#else
+  /* Dummy value. */
+  return Rf_ScalarLogical(FALSE);
+#endif
 }
 
 static const R_CallMethodDef usdt_entries[] = {
